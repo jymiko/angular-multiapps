0ac9f2a96b1db0bc36911b0c9d8405af0eb8a54e
jatmiko jatmiko <jatmiko.jatmiko@salt.co.id>
2023-12-18 10:42:02 +0700

    initial commit

--- projects/app1/src/app/app-routing.module.ts
+++ projects/app1/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'app1', loadChildren: () => import('../../projects/app1/src/app/app.module').then(m=>m.AppModule)}
+  {path: 'app1/home', loadChildren: () => import('./home/home-routing.module').then(m=>m.HomeRoutingModule)},
+  {path: 'app1', redirectTo: 'app1/home'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
